ab12551c82fe7d2b82d09e3108ffecf7a4c0d9c7
Thiago Sousa <thiagosousa81@gmail.com>
2023-09-19 23:53:42 -0300

    §

---
+++ Aula3.cpp
@@ -0,0 +1,22 @@
+// Declaração de variáveis
+int idade;
+float preco;
+char letra;
+bool esta_chovendo;
+std::string nome;
+
+// Declaração com Inicialização
+
+int numero = 42;
+float altura = 1.75;
+char letra_inicial = 'A';
+bool ligado = true;
+std::string nome = "Alice";
+
+// Inicialização posterior à declaração
+
+int idade;
+idade = 25;
+
+std::string sobrenome;
+sobrenome = "Silva";
